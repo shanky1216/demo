076062524c11c7585b8355addba3391f4c824469
Jatin Joshi <jatinjoshi905@gmail.com>
2025-03-18 10:46:04 +0530

    Slider added

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const menuToggle = document.querySelector('.mobile-menu-toggle');
 const navbar = document.querySelector('.home-main .top-navbar ul');
+const image = [
+    'url(/assets/bg-image.jpg)',
+    'url(/assets/background.jpg)',
+    'url(/assets/download (1).jpg)',
+    'url(/assets/download.jpg'
+];
+
+let currentIndex = 0;
+
+function changeImage(direction) {
+    currentIndex += direction;
+
+    if ( currentIndex < 0) {
+        currentIndex = image.length - 1;
+    } else if (currentIndex >= image.length) {
+        currentIndex = 0;
+    }
+
+    document.getElementById('migration-carausal').style.backgroundImage = image[currentIndex];
+}
+
+document.getElementById('migration-carausal').style.backgroundImage = image[currentIndex];
 
 menuToggle.addEventListener('click', function() {
     navbar.classList.toggle('show');
 });
+
